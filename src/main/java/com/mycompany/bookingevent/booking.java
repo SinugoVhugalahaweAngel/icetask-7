c1c3a6d1b44031a9255a2c9fff3d736e8162e7e4
Angel Vhugie <Angel Vhugie@DESKTOP-T6KBT5Q>
2024-10-02 08:34:32 +0200

    Added a check password complexity method to check if a particular  password has a specialcharacter,digit etc

--- src/main/java/com/mycompany/bookingevent/booking.java
+++ src/main/java/com/mycompany/bookingevent/booking.java
@@ -18,4 +18,30 @@ public class booking {
         // Ensure the username contains an underscore and is no more than 12 characters
         return code.length() <= 12 && code.contains("_");
     }
+    // This method ensures that password meet the following password complexity rules ,the password must be:Atleast 8 characters long ,Contains capital letter,Contains a number,Contains special character 
+    public boolean checkPasswordComplexity(String password) {
+        this.password = password;
+
+        // Flags for password complexity checks
+        boolean hasCapital = false;
+        boolean hasSpecialcharacter = false;
+        boolean hasDigit = false;
+
+          for (int i = 0; i < password.length(); i++) {
+        char c = password.charAt(i);  // Get character at index i
+
+        if (Character.isUpperCase(c)) {
+            hasCapital = true;
+        } else if (Character.isDigit(c)) {
+            hasDigit = true;
+        } else if (!Character.isLetterOrDigit(c)) {  // Check for special character
+            hasSpecialcharacter = true;
+        }
+          }
+           // At least 8 characters, a capital letter, a number, and a special character
+        return password.length() >= 8 && hasCapital && hasSpecialcharacter && hasDigit;
+    
+    }
+
+    
 }
