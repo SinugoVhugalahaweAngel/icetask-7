13b45852d6cb6a9336957c78d878ff72ed259ecc
Angel Vhugie <Angel Vhugie@DESKTOP-T6KBT5Q>
2024-10-02 08:19:34 +0200

    Added Java booking system with Scanner input and Booking class

---
+++ src/main/java/com/mycompany/bookingevent/BookingEvent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bookingevent;
+import java.util.Scanner;
+/**
+ *
+ * @author Angel Vhugie
+ */
+public class BookingEvent {
+
+    public static void main(String[] args) {
+         
+        // Create a Scanner object to read user input
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter their first name
+        System.out.println("Enter your booking code: ");
+        String bookingcode = sc.nextLine();  // Read first name from user input
+
+         // Create an instance of the Login class to handle registration and login
+        Booking login = new Booking();
+
+    }
+}
